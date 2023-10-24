41803c5049295b17d559d9ad47d4ae6a630487bb
Surbhi <singhsubi850@gmail.com>
2023-10-25 01:01:27 +0530

    hello world

---
+++ src/helo/hello1.java
@@ -0,0 +1,6 @@
+package helo;
+public class hello1{
+public static void main(String[] args){
+System.out.println("Hello world");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author surbh
+ *
+ */
+module jenkinsTrial {
+}
